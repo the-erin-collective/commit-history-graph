7486572583aacb38783e8ed0d6c690d9460e94c7
erin <fix.society.github@gmail.com>
2023-03-25 19:21:38 +0200

    use same settings file and ignore non-sample file

---
+++ src/commit-history-graph/local.settings.sample.js
@@ -0,0 +1,8 @@
+const env = {
+    isOrg : false,
+    nameLookup: 'your-user-name',
+    clientId: 'your-client-id',
+    clientSecret: 'your-client-secret'
+};
+
+module.exports = env;
--- src/commit-history-graph/next.config.js
+++ src/commit-history-graph/next.config.js
@@ -1,28 +1,50 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
-const localSettings = require('./local.settings');
+const fs = require('fs');
 
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  env: localSettings,
-  compress: false,
-  productionBrowserSourceMaps: true,
-  swcMinify: false,
-  webpack: (config,
-    {buildId, dev, isServer, defaultLoaders, nextRuntime, webpack}
-    ) => {
-      config.optimization.minimize = false;
-      config.resolve.alias.react = path.resolve('./node_modules/react');
-      config.module.rules.push(
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader', 'postcss-loader']
-        }
-      );
-      return config;
-  },
+const getLocalSettings = async () => {
+  try 
+  {
+    const localSettings = require('./local.settings');
+
+    return localSettings;
+  } 
+  catch (ex) 
+  {
+    const data = await fs.promises.readFile('./local.settings.sample.js');
+    const localSettings = data.toString();
+
+    await fs.promises.writeFile("./local.settings.js", localSettings); 
+    
+    return localSettings;
+  }
 }
 
-module.exports = nextConfig
+module.exports = async function() {
+  const localSettings = await getLocalSettings();
+  
+  const nextConfig = {
+    experimental: {
+      appDir: true,
+    },
+    env: localSettings,
+    compress: false,
+    productionBrowserSourceMaps: true,
+    swcMinify: false,
+    webpack: (config,
+      {buildId, dev, isServer, defaultLoaders, nextRuntime, webpack}
+      ) => {
+        config.optimization.minimize = false;
+        config.resolve.alias.react = path.resolve('./node_modules/react');
+        config.module.rules.push(
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader', 'postcss-loader']
+          }
+        );
+        return config;
+    },
+  }
+
+  return nextConfig;
+}
